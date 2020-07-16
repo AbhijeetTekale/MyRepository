16c84ba61643baa46c30a79d325c9ed5c7218867
AbhijeetTekale <55428001+AbhijeetTekale@users.noreply.github.com>
2020-07-16 10:34:23 +0530

    commit 1

---
+++ gradlew.bat
@@ -0,0 +1,48 @@
+<resources
+    xmlns:xliff="urn:oasis:names:tc:xliff:document:1.2">
+
+    <string name="app_name">Budget Manager</string>
+    <string name="icon">icon</string>
+    <string name="budgetsTitle">Budgets</string>
+    <string name="addBudgetTitle">Add Budget</string>
+    <string name="viewBudgetTitle">View Budget</string>
+    <string name="editBudgetTitle">Edit Budget</string>
+    <string name="budgetDescription">Create and manage budgets</string>
+    <string name="transactionsTitle">Transactions</string>
+    <string name="transactionsDescription">Enter transactions and revenues</string>
+    <string name="addExpenseTransactionTitle">Add Expense</string>
+    <string name="editExpenseTransactionTitle">Edit Expense</string>
+    <string name="viewExpenseTransactionTitle">View Expense</string>
+    <string name="addRevenueTransactionTitle">Add Revenue</string>
+    <string name="editRevenueTransactionTitle">Edit Revenue</string>
+    <string name="viewRevenueTransactionTitle">View Revenue</string>
+    <string name="importExportTitle">Import/Export</string>
+    <string name="totalBudgetTitle">Total</string>
+    <string name="addShortcutsTitle">Add Budget Manager Shortcut</string>
+    <string name="addExpenseTransactionShortcutTitle">Add Expense Shortcut</string>
+    <string name="addRevenueTransactionShortcutTitle">Add Revenue Shortcut</string>
+
+    <string name="add">Add</string>
+    <string name="selectDates">Select Dates</string>
+    <string name="type">Type</string>
+    <string name="value">Value</string>
+    <string name="name">Name</string>
+    <string name="budget">Budget</string>
+    <string name="account">Account</string>
+    <string name="note">Note</string>
+    <string name="date">Date</string>
+    <string name="save">Save</string>
+    <string name="cancel">Cancel</string>
+    <string name="set">Set</string>
+    <string name="receipt">Receipt</string>
+    <string name="capture">Capture</string>
+    <string name="view">View</string>
+    <string name="update">Update</string>
+    <string name="clean">Clean</string>
+    <string name="ok">OK</string>
+    <string name="share">Share</string>
+    <string name="confirm">Confirm</string>
+
+    <string name="Grocery">Grocery</string>
+    <string name="one_hundred">100</string>
+    <stri
